776cd3fa9d680149a1b95b6c2bbb44c5fdfbf778
KazuyaOomoto <n4p4yeah@gmail.com>
2019-02-24 21:03:54 +0900

    GoForwardUnityChan 初めてのコミット

---
+++ Assets/BackGroundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BackGroundController : MonoBehaviour {
+
+	private float scroll = -0.03f;
+
+	private float deadLine = -16.0f;
+
+	private float startLine = 15.8f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (this.scroll, 0, 0);
+
+		if(transform.position.x < this.deadLine){
+			transform.position = new Vector2 (this.startLine, 0);
+		}
+	}
+}
---
+++ Assets/CubeController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+public class CubeController : MonoBehaviour {
+
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			// キューブを移動させる
+			this.transform.Translate(this.speed, 0, 0);
+
+			// 画面の外に出たら破棄する
+			if (this.transform.position.x < this.deadLine) {
+				Destroy (this.gameObject);
+			}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		/* 衝突対象が自身または地面	*/
+		if (collision.gameObject.tag == "CubeTag" || collision.gameObject.tag == "GroundTag")
+			/* 効果音を鳴らす	*/
+			gameObject.GetComponent<AudioSource> ().Play ();
+	}
+}
